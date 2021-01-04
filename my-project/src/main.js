ef7053dd93168a0e4cd1db20d1dd45be29ae775c
singsing215 <56101581+singsing215@users.noreply.github.com>
2021-01-04 14:33:28 +0800

    create vue router

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter(
+  {
+    routes: routes
+  }
+);
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+});
+
