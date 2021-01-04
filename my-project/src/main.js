8e781c2594a575b605ca012b1bc8f43bca42c1e5
singsing215 <56101581+singsing215@users.noreply.github.com>
2021-01-04 14:44:56 +0800

    router

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,23 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import router from './router'
 
-Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-const router = new VueRouter(
-  {
-    routes: routes
-  }
-);
 
 new Vue({
   el: '#app',
